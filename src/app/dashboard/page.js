c2cece1e0a29da2995a9c065d1d983a95eb5234a
Ryabbb <ryableonard@gmail.com>
2024-06-28 17:44:25 -0400

    removed deuplicate navbar

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,9 @@
 
 import React from "react";
-import Navbar from "../components/NavBar";
 
 function Dashboard() {
   return (
     <div>
-      <Navbar />
       <h1>Dashboard</h1>
     </div>
   );
