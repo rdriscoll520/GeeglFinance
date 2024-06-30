3ae0919dac446bd185ecb0c4fa9145ad5d5e6635
Ryabbb <ryableonard@gmail.com>
2024-06-29 22:04:46 -0400

    Improved layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,40 +3,39 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/NavBar';
-import Sidebar from './components/Sidebar';
-import { SidebarProvider, useSidebar } from './components/SidebarContext';
-import { useState, useEffect } from 'react'
-
+import Sidebar, { SidebarProvider, useSidebar } from './components/Sidebar';
+import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children, title }) {
+  const [isClient, setIsClient] = useState(false);
 
-  const [isClient, setIsClient] = useState(false)
- 
   useEffect(() => {
-    setIsClient(true)
-  }, [])
+    setIsClient(true);
+  }, []);
 
-  return (isClient?
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title}</title>
-      </head>
-      <body className={inter.className}>
-        <SidebarProvider>
-          <Content title={title}> 
-            {children}
-          </Content>
-        </SidebarProvider>
-      </body>
-    </html>
-  :<p></p>);
+  return (
+    isClient ? (
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>{title}</title>
+        </head>
+        <body className={inter.className}>
+          <SidebarProvider>
+            <Content title={title}>
+              {children}
+            </Content>
+          </SidebarProvider>
+        </body>
+      </html>
+    ) : <p></p>
+  );
 }
 
-function Content({ children, title }) { //container for page-specific content
+function Content({ children, title }) { 
   const { isSidebarOpen } = useSidebar();
 
   return (
@@ -45,7 +44,7 @@ function Content({ children, title }) { //container for page-specific content
       <Sidebar />
       <main
         style={{
-          marginLeft: isSidebarOpen ? '240px' : '40px',
+          marginLeft: isSidebarOpen ? '200px' : '0px',
           transition: 'margin-left 0.3s ease-in-out',
           padding: '20px',
         }}
